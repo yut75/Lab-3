2d0be2f5e0e9cba0801e6f82892a4339d0ed7678
yuteng <yuteng.25@ichat.sp.edu.sg>
2026-05-05 16:20:31 +0800

    gggggggggggg

--- test_BMI.py
+++ test_BMI.py
@@ -1,4 +1,13 @@
 import Lab2.BMI as bmi
 
-def test_bmi_normal_weight()
-    result = bmi.calculate_bmi
+def test_bmi_under_weight():
+    result = bmi.calculate_bmi(1.70, 40)
+    assert result == -1
+
+def test_bmi_normal_weight():
+    result = bmi.calculate_bmi(1.70, 60)
+    assert result == 0
+
+def test_bmi_over_weight():
+    result = bmi.calculate_bmi(1.70, 100)
+    assert result == 1
